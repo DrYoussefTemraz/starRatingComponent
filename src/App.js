fe305104114029a6d6ecc8bd71e15ba9a12d23aa
DrYoussefTemraz <134495550+DrYoussefTemraz@users.noreply.github.com>
2024-07-10 22:48:31 +0000

    restore the original star rating

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import StarRating from "./star-rating";
 function App() {
   return (
     <div className="App">
-      <a href="https://github.com/DrYoussefTemraz/starRatingComponent">
-        learn react
-      </a>
       <p>please make star rating </p>
       <StarRating noOfStars={10} />
     </div>
--- src/Components/Greet/Greet.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-type GreetProps = {
-  name?: string;
-};
-export const Greet = (props: GreetProps) => {
-  return <div>Hello {props.name}</div>;
-};
